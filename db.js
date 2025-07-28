ec8ce5d14ba0b9a8682898b1ce43177f8f87317a
Samhndii <Samhndiii@gmail.com>
2025-07-28 16:20:18 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '159635741?Ny',
+  password: 'yourMySqlPassword',
   database: 'budget_db'
 });
 
